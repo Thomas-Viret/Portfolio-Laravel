65c06850a4dc6c83e59c280fd04cb18667993384
Thomas Viret <th.viret@gmail.com>
2021-05-20 22:09:19 +0200

    v1 ok

--- resources/views/presentation.blade.php
+++ resources/views/presentation.blade.php
@@ -23,13 +23,18 @@
                                                         <p>Bonjour, </p>
                                                         <p>Je suis Thomas Viret et je suis Développeur Web</p>
 
+                                                        <p>J'ai terminé ma formation ce qui marque le début de l'aventure </p>
+
+                                                        <p>Fort de ma passion pour le développement je me suis découvert un intérêt particulier pour le backend</p>
+
+                                                        <p>Ce qui a orienté mon choix de la spécialisation Symfony </p>
+
                                                         <p>À l'origine je suis Educateur Spécialisé </p>
 
                                                         <p>mais je me suis dit que le social, le contact et les relations ont évolué alors pourquoi pas moi ???</p>
 
-                                                        <p>Je viens de terminer ma formation ce qui marque le début de l'aventure </p>
-
-                                                        <p>J'ai énormément apprécié apprendre le développement web, en particulier le backend</p>
+                                                        <p>En tout cas mes expériences précédentes durant 13 ans dans le secteur du handicap </p>
+                                                        <p>m’ont permis d’acquérir des compétences relationnelles, d’organisation et d’adaptation </p>
 
                                                         <p>Je vous invite à continuer votre visite et en décourivr un peu plus sur moi 😉 </p>
 
--- resources/views/skills.blade.php
+++ resources/views/skills.blade.php
@@ -17,7 +17,7 @@
                                                         <span></span>
                                                         <span></span>
                                                         <div class="content--box">
-                                                        <h2>Skills</h2>
+                                                        <h2>HARD SKILLS</h2>
                                                         <p>HTML 5</p>
                                                         <p>CSS 3</p>
 
@@ -32,6 +32,24 @@
                                                         <p>Bootstrap</p>
 
                                                         <p>Wordpress</p>
+
+                                                        <h2>SOFT SKILLS</h2>
+
+                                                        <p>Rigueur</p>
+
+                                                        <p>Autonomie</p>
+
+                                                        <p>Adaptabilité</p>
+
+                                                        <p>Sociabilité</p>
+
+                                                        <p>Organisation</p>
+
+                                                        <p>Percévérance</p>
+
+                                                        <p>Sérieux</p>
+
+
 
                                                         </div>
 
